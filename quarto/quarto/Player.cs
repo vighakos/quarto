2f8608f8cfb2730c560176b6f0712a470e4122e9
vighakos <vighakos@turr.hu>
2022-11-18 11:21:54 +0100

    classok

---
+++ quarto/quarto/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quarto
+{
+    class Player
+    {
+        public string Name, Color;
+        public int Victories;
+
+        public Player(string name, string color)
+        {
+            Name = name;
+            Color = color;
+            Victories = 0;
+        }
+    }
+}
